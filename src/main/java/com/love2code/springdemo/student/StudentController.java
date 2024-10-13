05ded44c095d5175bf6bab43b23c8e780357284e
Sathya1312 <sathyapriya1312@gmail.com>
2024-10-13 11:08:30 +0530

    implementing service layer

--- src/main/java/com/love2code/springdemo/student/StudentController.java
+++ src/main/java/com/love2code/springdemo/student/StudentController.java
@@ -4,18 +4,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/students")
 public class StudentController {
+        
+    private StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
 
     @GetMapping
-    public List<String> findAllStudents(){
-        return List.of(
-                "Sathya Priya",
-                "Hello World",
-                "Spring boot"
-        );
+    public List<Student> findAllStudents(){
+        return studentService.findAllStudents();
     }
 }
