6c9d9e5192f6416bd1fdad82181b7e7329b5f46b
Sathya1312 <sathyapriya1312@gmail.com>
2024-10-16 18:08:39 +0530

    implementing service layer

---
+++ src/main/java/com/love2code/springdemo/student/StudentService.java
@@ -0,0 +1,29 @@
+package com.love2code.springdemo.student;
+
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> findAllStudents(){
+        return List.of(
+                new Student(
+                "Sathya",
+                "Priya",
+                LocalDate.now(),
+                "contact@sathya.com",
+                34
+        ),
+        new Student(
+                "Anand",
+                "Shankar",
+                LocalDate.now(),
+                "contact@anand.com",
+                34
+        )
+        );
+    }
+}
